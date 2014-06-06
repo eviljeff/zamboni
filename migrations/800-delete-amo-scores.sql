bdcd084d9fb42726f587dfa44c8b2dad04cbc4e9
eviljeff <awilliamson@mozilla.com>
2014-06-06 18:35:16 +0100

    Clear out AMO reviewer scores
    They're stale but are included in the performance tables on Marketplace.

---
+++ migrations/800-delete-amo-scores.sql
@@ -0,0 +1,3 @@
+DELETE FROM from reviewer_scores where note_key IN (10,11,12,20,21,22,30,31,32,40,50,51,52,60,61,62,80);
+
+# see https://github.com/mozilla/zamboni/blob/master/apps/constants/base.py#L399:L422
